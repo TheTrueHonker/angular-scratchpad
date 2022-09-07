a9b918ee16206b99dc6559a6e2cde12e416fe7df
Daniel Wemmer <daniel.wemmer.dw@gmail.com>
2022-09-07 20:01:17 +0200

    feat: add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateNotesComponent } from './create-notes/create-notes.component';
+import { HomeComponent } from './home/home.component';
+import { NotesComponent } from './notes/notes.component';
+import { RegisterLoginComponent } from './register-login/register-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'notes', component: NotesComponent},
+  {path: 'create-notes', component: CreateNotesComponent},
+  {path: 'register-login', component: RegisterLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NotesComponent } from './notes/notes.component';
+import { CreateNotesComponent } from './create-notes/create-notes.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterLoginComponent } from './register-login/register-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NotesComponent,
+    CreateNotesComponent,
+    HomeComponent,
+    RegisterLoginComponent,
   ],
   imports: [
     BrowserModule,
