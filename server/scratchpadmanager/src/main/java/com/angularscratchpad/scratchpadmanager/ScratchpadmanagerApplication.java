bda68632475c74d7a10395901c62f48af49982f6
Daniel Wemmer <daniel.wemmer.dw@gmail.com>
2022-09-08 23:25:31 +0200

    feat: add server to project
     - Move client to seperate folder
     - Add spring server with maven

---
+++ server/scratchpadmanager/src/main/java/com/angularscratchpad/scratchpadmanager/ScratchpadmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.angularscratchpad.scratchpadmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScratchpadmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScratchpadmanagerApplication.class, args);
+	}
+
+}
---
+++ server/scratchpadmanager/src/test/java/com/angularscratchpad/scratchpadmanager/ScratchpadmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularscratchpad.scratchpadmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScratchpadmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
